edb177c1b55a01c05ec5f3b875f00f0b6dd532fb
akms-limon <akmslimon@gmail.com>
2023-06-18 02:34:40 +0600

    a

--- Algebra/Binary Exponentiation/BinaryExponentiation.cpp
+++ Algebra/Binary Exponentiation/BinaryExponentiation.cpp
@@ -5,8 +5,20 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+bitset<1000000001>arr;
+
 int main() {
     
-      
+    int k = -1;
+    for(int i = 2; i<=100000000; i++){
+        if(!arr[i]){
+            if(++k%100==0)
+                cout<<i<<endl;
+            if(i<=10000)
+            for(int j = 2; i*j<=100000000; j++){
+                arr[i*j]=true;
+            }
+        }
+    }
     return 0;
 }
