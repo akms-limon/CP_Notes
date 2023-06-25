4eceab77ce937886810fa1a0f7d6a9d8170773cf
akms-limon <akmslimon@gmail.com>
2023-06-25 13:47:37 +0600

    25 june 2023

--- Bit Manipulation/56_Bitmask_Codeforces_A_Array_Elimination.cpp
+++ Bit Manipulation/56_Bitmask_Codeforces_A_Array_Elimination.cpp
@@ -19,7 +19,7 @@ int main(){
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < 32; j++) {
                 if((a[i]>>j)&1){
-                    v[j]++;
+                    v[j]++; 
                 }
             }
         }
