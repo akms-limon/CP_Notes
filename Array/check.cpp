7ee0eb73002210b3492c3222e3419b6d414db484
akms-limon <akmslimon@gmail.com>
2024-01-27 00:20:51 +0600

    a

---
+++ Array/check.cpp
@@ -0,0 +1,14 @@
+/**
+ *    author: A K M S Limon
+ *    created: 27-January-2024  00:20:02
+**/
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+    
+    return 0;
+}
