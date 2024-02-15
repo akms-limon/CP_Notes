ac1a7fbb4d47e57d0f4c312baafbaf03994ab680
akms-limon <akmslimon@gmail.com>
2024-02-16 02:27:11 +0600

    new

---
+++ Algebra/Gcd Lcm/HowManyDigitofAFactorial.cpp
@@ -0,0 +1,14 @@
+/**
+ *    author: A K M S Limon
+ *    created: 16-February-2024  02:24:51
+**/
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+    // how many digit in 10e5! ? double ans = (double)log(10) 1 + (double)log(10) 2 + (double) log(10) 3 + .... + (double) log(10) n 
+    return 0;
+}
